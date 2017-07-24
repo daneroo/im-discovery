5fe6ff8b85f6777ba2e530758a217ea03758b0df
Daniel Lauzon <daniel.lauzon@irdeto.com>
2017-07-24 00:58:16 -0400

    Start with jsipfs

--- README.md
+++ README.md
@@ -1,8 +1,34 @@
 # Exploration of ipfs and libp2p discovery
 
 ## TODO
-- chat example
+- two nodes with pubsub
 - structure with [aegir](https://github.com/ipfs/aegir)
+
+## Docker
+```
+docker build -t disco .
+docker run --rm -it --name disco1 disco
+docker run --rm -it --name disco2 disco
+```
+
+Try pubsub through jsipfs
+```
+docker run --rm -it --name disco2 bash
+alias jsipfs=./node_modules/.bin/jsipfs
+jsipfs init
+sleep 2
+jsipfs config enablePubsubExperiment true
+jsipfs config enable-pubsub-experiment true
+sleep 2
+jsipfs daemon &
+#DEBUG=*,-mplex:* jsipfs daemon &
+sleep 3
+echo "I <3 IPFS -$(whoami)@$(hostname) $(date -Iseconds)" >content
+hash=$(jsipfs add content|cut -d\  -f 2)
+echo curl "https://ipfs.io/ipfs/$hash"
+curl "https://ipfs.io/ipfs/$hash"
+
+```
 
 ## References
 - [Implementations](https://libp2p.io/implementations/)
--- index.js
+++ index.js
@@ -1,21 +1,77 @@
 
-// not working yet
-// no deps
-const PeerInfo = require('peer-info')
-const Node = require('./libp2p-ipfs-nodejs')
-PeerInfo.create((err, pi) => {
-  if (err) {
-    throw err // handle the err
+const IPFS = require('ipfs')
+
+const sleep = ms => new Promise(resolve => setTimeout(resolve, ms))
+
+const ipfs = new IPFS({
+  // repo: repo,
+  // init: true, // default
+  // init: false,
+  // init: {
+  //   bits: 1024 // size of the RSA key generated
+  // },
+  // start: true,
+  // start: false,
+  EXPERIMENTAL: { // enable experimental features
+    pubsub: true,
+    sharding: true // enable dir sharding
+    // dht: true // enable KadDHT, currently not interopable with go-ipfs
   }
-
-  // this throws!!!
-  pi.multiaddr.add('/ip4/0.0.0.0/tcp/0')
-
-  const node = new Node(pi)
-  node.start((err) => {
-    if (err) {
-      throw err // handle the err
-    }
-    console.log('Node is ready o/')
-  })
+  // config: { // overload the default IPFS node config
+  //   Addresses: {
+  //     Swarm: [
+  //       '/ip4/127.0.0.1/tcp/1337'
+  //     ]
+  //   }
+  // },
+  // libp2p: { // add custom modules to the libp2p stack of your node
+  //   modules: {}
+  // }
 })
+
+ipfs.on('ready', async () => {
+  console.log(`-ready`)
+  const id = await ipfs.id()
+  console.log(`+ready (${id.id})`)
+})    // Node is ready to use when you first create it
+
+ipfs.on('error', (err) => {
+  console.log('error', err)
+}) // Node has hit some error while initing/starting
+
+ipfs.on('init', () => {
+  console.log('init')
+})     // Node has successfully finished initing the repo
+
+ipfs.on('start', async () => {
+  console.log('-start')
+  const id = await ipfs.id()
+  console.log(`+start (${id.id})`)
+
+  await ipfs.pubsub.subscribe('scrobble', {}, (msg) => {
+    // {from: string, seqno: Buffer, data: Buffer, topicCIDs: Array<string>}
+    const from = msg.from()
+    console.log(`subscribe: from ${from}`)
+  })
+  console.log(`+subscribed (${id.id})`)
+
+  setInterval(async () => {
+    await showSwarm(id)
+  }, 5000)
+})    // Node has started
+
+ipfs.on('stop', () => {
+  console.log('stop')
+})     // Node has stopped
+
+async function showSwarm (myId) {
+  console.log('-swarm')
+  const peers = await ipfs.swarm.peers()
+  console.log(`|peers|=${peers.length}`)
+  for (let peer of peers) {
+    const id = peer.peer.id.toB58String()
+    const addr = peer.addr.toString()
+    console.log(` peer: ${addr} (${id})`)
+  }
+  console.log(`+swarm (${myId.id})`)
+}
